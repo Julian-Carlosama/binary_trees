3d3ed482bb2bec422856acd2cb3176ee009523d5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-02 02:03:31 -0500

    V9 Update betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,9 +12,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	/*else
-	{
-		return (binary_tree_size(tree->left) + binary_tree_size(tree->right));
-		}*/
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
