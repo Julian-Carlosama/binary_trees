9bfe9a958628f793404820a1edaf50d9bbd67c08
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-02 01:59:30 -0500

    V8 Update function

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,9 +10,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
-	else if ((node->left != NULL) && (node->right != NULL))
-		return (1);
-	return (0);
+	/*if ((node->left != NULL) && (node->right != NULL))
+	  return (1);*/
+	return (1);
 }
