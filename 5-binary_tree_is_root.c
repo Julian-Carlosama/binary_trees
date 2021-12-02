f7655272ec1cc0aa561491c4c9ef8dd42cafdd84
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-02 02:31:18 -0500

    V10 Update betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
 		return (0);
-	/*if ((node->left != NULL) && (node->right != NULL))
-	  return (1);*/
 	return (1);
 }
