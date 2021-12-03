3d270d4e1e765236a40e84d08a98ac6d3056e5a4
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-02 22:24:50 -0500

    V16 new function

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Function that goes through a binary
+ * tree using post-order traversal.
+ *
+ * @tree: Pointer to the root node of the tree to traverse.
+ *
+ * @func: Pointer to function to call for each node.
+ *
+ * Return: None.
+ */
+
+void  binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
